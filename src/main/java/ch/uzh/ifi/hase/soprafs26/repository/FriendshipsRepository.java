b00b65cc51830ed87f1b5e223ed73171c699269c
fraiaperezrayonforsman-cloud <fraia.perezrayonforsman@uzh.ch>
2026-03-26 16:59:45 +0100

    fix improper mapping between user and friendship #50

--- src/main/java/ch/uzh/ifi/hase/soprafs26/repository/FriendshipsRepository.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/repository/FriendshipsRepository.java
@@ -8,5 +8,6 @@ import ch.uzh.ifi.hase.soprafs26.entity.User;
 
 @Repository("friendshipsRepository")
 public interface FriendshipsRepository extends JpaRepository<Friendships, Long> {
+	@Query("SELECT f FROM Friendships f WHERE f.user = ?1")
 	Friendships findByUser(User user);
 }
