b9c0f1b3d8c9ca7088783361cdb057b09e789e6c
fraiaperezrayonforsman-cloud <fraia.perezrayonforsman@uzh.ch>
2026-04-14 11:25:14 +0200

    included two new methods in SessionService, namely joinSession and leaveSession which set the joinedAt and leftAt attributes of SessionParticipant entity. Depending on this, the points are calculated and added to each particiapants instance in leaderboard repository. Additionally, another method was added in SessionService to delete sessions older than 30 days. For this, scheduling imports and annotations were added to Application. Both endpoints, joinSession and leaveSession were added to SessionController #63

--- src/main/java/ch/uzh/ifi/hase/soprafs26/repository/SessionRepository.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/repository/SessionRepository.java
@@ -5,6 +5,10 @@ import org.springframework.stereotype.Repository;
 
 import ch.uzh.ifi.hase.soprafs26.entity.Session;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository("sessionRepository")
 public interface SessionRepository extends JpaRepository<Session, Long> {
+    List<Session> findByEndTimeBefore(LocalDateTime cutoff);
 }
---
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/rest/dto/SessionLeavePostDTO.java
@@ -0,0 +1,14 @@
+package ch.uzh.ifi.hase.soprafs26.rest.dto;
+
+public class SessionLeavePostDTO {
+
+    private Long pagesRead;
+
+    public Long getPagesRead() {
+        return pagesRead;
+    }
+
+    public void setPagesRead(Long pagesRead) {
+        this.pagesRead = pagesRead;
+    }
+}
